d1d62686e7feb18636eca8c741544cb5a4cc1254
Per Ivar Nerseth <perivar@nerseth.com>
2018-11-13 22:55:58 +0100

    eq fix

--- Eq.cs
+++ Eq.cs
@@ -9,14 +9,14 @@ namespace AbletonLiveConverter
     {
         public enum BandMode
         {
-            LowCut4,
-            LowCut,
-            HighShelf,
-            Peak,
+            LowCut48,
+            LowCut12,
+            LeftShelf,
+            Bell,
             Notch,
-            LowShelf,
-            HighCut,
-            HighCut4
+            RightShelf,
+            HighCut12,
+            HighCut48
         }
 
         public Eq(XElement xelement)
@@ -36,7 +36,7 @@ namespace AbletonLiveConverter
         private void ParseBand(XElement band, string parameter)
         {
             var isOn = band.Descendants(parameter).Descendants("IsOn").Descendants("Manual").Attributes("Value").First().Value.Equals("true");
-            var mode = (BandMode) int.Parse(band.Descendants(parameter).Descendants("Mode").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
+            var mode = (BandMode)int.Parse(band.Descendants(parameter).Descendants("Mode").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
             var freq = float.Parse(band.Descendants(parameter).Descendants("Freq").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
             var gain = float.Parse(band.Descendants(parameter).Descendants("Gain").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
             var q = float.Parse(band.Descendants(parameter).Descendants("Q").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
