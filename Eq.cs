3faf59998ff4839658780b5055101f2bd246c3a2
Per Ivar Nerseth <perivar@nerseth.com>
2018-11-13 22:07:03 +0100

    Fix eq

--- Eq.cs
+++ Eq.cs
@@ -7,6 +7,18 @@ namespace AbletonLiveConverter
 {
     public class Eq
     {
+        public enum BandMode
+        {
+            LowCut4,
+            LowCut,
+            HighShelf,
+            Peak,
+            Notch,
+            LowShelf,
+            HighCut,
+            HighCut4
+        }
+
         public Eq(XElement xelement)
         {
             var bands = from d in xelement.Descendants()
@@ -15,35 +27,24 @@ namespace AbletonLiveConverter
 
             foreach (var band in bands)
             {
-                var isOnA = band.Descendants("ParameterA").Descendants("IsOn").Descendants("Manual").Attributes("Value").First().Value.Equals("true");
-                var freqA = float.Parse(band.Descendants("ParameterA").Descendants("Freq").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
-                var gainA = float.Parse(band.Descendants("ParameterA").Descendants("Gain").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
-                var qA = float.Parse(band.Descendants("ParameterA").Descendants("Q").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
-
-                if (isOnA && gainA != 0)
-                {
-                    Console.WriteLine("ParameterA: {0:0.00} Hz, Gain: {1:0.00}, Q: {2:0.00}, On: {3}", freqA, gainA, qA, isOnA);
-                }
-                else
-                {
-                    // Console.WriteLine("OFF ParameterA: {0:0.00} Hz, Gain: {1:0.00}, Q: {2:0.00}, On: {3}", freqA, gainA, qA, isOnA);
-                }
-
-                var isOnB = band.Descendants("ParameterB").Descendants("IsOn").Descendants("Manual").Attributes("Value").First().Value.Equals("true");
-                var freqB = float.Parse(band.Descendants("ParameterB").Descendants("Freq").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
-                var gainB = float.Parse(band.Descendants("ParameterB").Descendants("Gain").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
-                var qB = float.Parse(band.Descendants("ParameterB").Descendants("Q").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
-
-                if (isOnB && gainB != 0)
-                {
-                    Console.WriteLine("ParameterB: {0:0.00} Hz, Gain: {1:0.00}, Q: {2:0.00}, On: {3}", freqB, gainB, qB, isOnB);
-                }
-                else
-                {
-                    // Console.WriteLine("OFF ParameterB: {0:0.00} Hz, Gain: {1:0.00}, Q: {2:0.00}, On: {3}", freqB, gainB, qB, isOnB);
-                }
+                ParseBand(band, "ParameterA");
+                // ParseBand(band, "ParameterB");
             }
             Console.WriteLine();
+        }
+
+        private void ParseBand(XElement band, string parameter)
+        {
+            var isOn = band.Descendants(parameter).Descendants("IsOn").Descendants("Manual").Attributes("Value").First().Value.Equals("true");
+            var mode = (BandMode) int.Parse(band.Descendants(parameter).Descendants("Mode").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
+            var freq = float.Parse(band.Descendants(parameter).Descendants("Freq").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
+            var gain = float.Parse(band.Descendants(parameter).Descendants("Gain").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
+            var q = float.Parse(band.Descendants(parameter).Descendants("Q").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
+
+            if (isOn)
+            {
+                Console.WriteLine(parameter + ": {0:0.00} Hz, Gain: {1:0.00} dB, Q: {2:0.00}, Mode: {3}", freq, gain, q, mode);
+            }
         }
     }
 }
