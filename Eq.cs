f1ee84baf76858b09435faaf37658873ed2d81f8
Per Ivar Nerseth <perivar@nerseth.com>
2018-11-13 01:03:41 +0100

    Added ADV parsing of Eq

---
+++ Eq.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace AbletonLiveConverter
+{
+    public class Eq
+    {
+        public Eq(XElement xelement)
+        {
+            var bands = from d in xelement.Descendants()
+                        where d.Name.LocalName.Contains("Bands")
+                        select d;
+
+            foreach (var band in bands)
+            {
+                var isOnA = band.Descendants("ParameterA").Descendants("IsOn").Descendants("Manual").Attributes("Value").First().Value.Equals("true");
+                var freqA = float.Parse(band.Descendants("ParameterA").Descendants("Freq").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
+                var gainA = float.Parse(band.Descendants("ParameterA").Descendants("Gain").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
+                var qA = float.Parse(band.Descendants("ParameterA").Descendants("Q").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
+
+                if (isOnA && gainA != 0)
+                {
+                    Console.WriteLine("ParameterA: {0:0.00} hz, gain: {1:0.00}, q: {2:0.00}, On: {3}", freqA, gainA, qA, isOnA);
+                }
+                else
+                {
+                    // Console.WriteLine("OFF ParameterA: {0:0.00} hz, gain: {1:0.00}, q: {2:0.00}, On: {3}", freqA, gainA, qA, isOnA);
+                }
+
+                var isOnB = band.Descendants("ParameterB").Descendants("IsOn").Descendants("Manual").Attributes("Value").First().Value.Equals("true");
+                var freqB = float.Parse(band.Descendants("ParameterB").Descendants("Freq").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
+                var gainB = float.Parse(band.Descendants("ParameterB").Descendants("Gain").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
+                var qB = float.Parse(band.Descendants("ParameterB").Descendants("Q").Descendants("Manual").Attributes("Value").First().Value, CultureInfo.InvariantCulture);
+
+                if (isOnB && gainB != 0)
+                {
+                    Console.WriteLine("ParameterB: {0:0.00} hz, gain: {1:0.00}, q: {2:0.00}, On: {3}", freqB, gainB, qB, isOnB);
+                }
+                else
+                {
+                    // Console.WriteLine("OFF ParameterB: {0:0.00} hz, gain: {1:0.00}, q: {2:0.00}, On: {3}", freqB, gainB, qB, isOnB);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace AbletonLiveConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFolder = @"C:\Users\perner\Amazon Drive\Documents\My Projects\Ableton";
+
+            string[] files = Directory.GetFiles(inputFolder, "*.adv", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine("Processing {0} ...", file);
+                byte[] bytes = File.ReadAllBytes(file);
+                byte[] decompressed = Decompress(bytes);
+                string str = Encoding.UTF8.GetString(decompressed);
+
+                XElement xelement = XElement.Parse(str);
+
+                // find preset type
+                var presetType = xelement.Elements().First().Name.ToString();
+                switch (presetType)
+                {
+                    case "Eq8":
+                        var eq = new Eq(xelement);
+                        break;
+                    case "AutoFilter":
+                    case "Compressor2":
+                    case "GlueCompressor":
+                    case "MultibandDynamics":
+                    case "Reverb":
+                    case "Saturator":
+                    case "Tuner":
+                        Console.WriteLine("Reading {0}", presetType);
+                        break;
+                    default:
+                        Console.WriteLine("{0} not supported!", presetType);
+                        break;
+                }
+            }
+        }
+
+        static byte[] Decompress(byte[] gzip)
+        {
+            // Create a GZIP stream with decompression mode.
+            // ... Then create a buffer and write into while reading from the GZIP stream.
+            using (GZipStream stream = new GZipStream(new MemoryStream(gzip),
+                CompressionMode.Decompress))
+            {
+                const int size = 4096;
+                byte[] buffer = new byte[size];
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = stream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+                    while (count > 0);
+                    return memory.ToArray();
+                }
+            }
+        }
+    }
+}
