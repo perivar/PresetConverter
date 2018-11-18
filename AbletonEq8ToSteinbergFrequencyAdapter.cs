1a9ff1bae0ef63c7f77be25b6081760dde7aac36
Per Ivar Nerseth <perivar@nerseth.com>
2018-11-18 17:40:53 +0100

    fixed band on off

--- AbletonEq8ToSteinbergFrequencyAdapter.cs
+++ AbletonEq8ToSteinbergFrequencyAdapter.cs
@@ -20,7 +20,7 @@ namespace AbletonLiveConverter
                 if (band.Parameter.Equals("ParameterA"))
                 {
                     int bandNumber = band.Number + 1; // zero indexed
-                    frequency.Parameters[String.Format("equalizerAon{0}", bandNumber)].Value = band.IsOn ? 1.00 : 0.00;
+                    frequency.Parameters[String.Format("equalizerAbandon{0}", bandNumber)].Value = band.IsOn ? 1.00 : 0.00;
                     frequency.Parameters[String.Format("equalizerAgain{0}", bandNumber)].Value = band.Gain;
                     frequency.Parameters[String.Format("equalizerAfreq{0}", bandNumber)].Value = band.Freq;
                     frequency.Parameters[String.Format("equalizerAq{0}", bandNumber)].Value = band.Q;
