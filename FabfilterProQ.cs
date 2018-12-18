0dd61b001340178bc0f4a6498307e80f29caff18
Per Ivar Nerseth <perivar@nerseth.com>
2018-12-18 19:39:52 +0100

    Added more EQ types

---
+++ FabfilterProQ.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using CommonUtils;
+
+namespace PresetConverter
+{
+    /// <summary>
+    /// FabfilterProQ Preset Class for saving a Fabfilter ProQ Preset file (fft)
+    /// </summary>
+    public static class FabfilterProQ
+    {
+        public static bool Convert2FabfilterProQ(REWEQFilters filters, string filePath)
+        {
+            List<ProQBand> proQBands = new List<ProQBand>();
+            foreach (REWEQBand filter in filters)
+            {
+                ProQBand band = new ProQBand();
+                band.FilterFreq = filter.FilterFreq;
+                band.FilterGain = filter.FilterGain;
+                band.FilterQ = filter.FilterQ;
+                band.Enabled = filter.Enabled;
+                switch (filter.FilterType)
+                {
+                    case REWEQFilterType.PK:
+                        band.FilterType = ProQFilterType.Bell;
+                        break;
+                    case REWEQFilterType.LP:
+                        band.FilterType = ProQFilterType.HighCut;
+                        break;
+                    case REWEQFilterType.HP:
+                        band.FilterType = ProQFilterType.LowCut;
+                        break;
+                    case REWEQFilterType.LS:
+                        band.FilterType = ProQFilterType.LowShelf;
+                        break;
+                    case REWEQFilterType.HS:
+                        band.FilterType = ProQFilterType.HighShelf;
+                        break;
+                    default:
+                        band.FilterType = ProQFilterType.Bell;
+                        break;
+                }
+                band.FilterLPHPSlope = ProQLPHPSlope.Slope24dB_oct;
+                band.FilterStereoPlacement = ProQStereoPlacement.Stereo;
+
+                proQBands.Add(band);
+            }
+
+            BinaryFile binFile = new BinaryFile(filePath, BinaryFile.ByteOrder.LittleEndian, true);
+            binFile.Write("FPQr");
+            binFile.Write((int)2);
+            binFile.Write((int)180);
+            binFile.Write((float)proQBands.Count);
+
+            for (int i = 0; i < 24; i++)
+            {
+                if (i < proQBands.Count)
+                {
+                    binFile.Write((float)FreqConvert(proQBands[i].FilterFreq));
+                    binFile.Write((float)proQBands[i].FilterGain);
+                    binFile.Write((float)QConvert(proQBands[i].FilterQ));
+                    binFile.Write((float)proQBands[i].FilterType);
+                    binFile.Write((float)proQBands[i].FilterLPHPSlope);
+                    binFile.Write((float)proQBands[i].FilterStereoPlacement);
+                    binFile.Write((float)(proQBands[i].Enabled ? 1 : 0));
+                }
+                else
+                {
+                    binFile.Write((float)FreqConvert(1000));
+                    binFile.Write((float)0);
+                    binFile.Write((float)QConvert(1));
+                    binFile.Write((float)ProQFilterType.Bell);
+                    binFile.Write((float)ProQLPHPSlope.Slope24dB_oct);
+                    binFile.Write((float)ProQStereoPlacement.Stereo);
+                    binFile.Write((float)1);
+                }
+            }
+
+            binFile.Write((float)0); // float output_gain;      // -1 to 1 (- Infinity to +36 dB , 0 = 0 dB)
+            binFile.Write((float)0); // float output_pan;       // -1 to 1 (0 = middle)
+            binFile.Write((float)2); // float display_range;    // 0 = 6dB, 1 = 12dB, 2 = 30dB, 3 = 3dB
+            binFile.Write((float)0); // float process_mode;     // 0 = zero latency, 1 = lin.phase.low - medium - high - maximum
+            binFile.Write((float)0); // float channel_mode;     // 0 = Left/Right, 1 = Mid/Side
+            binFile.Write((float)0); // float bypass;           // 0 = No bypass
+            binFile.Write((float)0); // float receive_midi;     // 0 = Enabled?
+            binFile.Write((float)3); // float analyzer;         // 0 = Off, 1 = Pre, 2 = Post, 3 = Pre+Post
+            binFile.Write((float)1); // float analyzer_resolution;  // 0 - 3 : low - medium[x] - high - maximum
+            binFile.Write((float)2); // float analyzer_speed;   // 0 - 3 : very slow, slow, medium[x], fast
+            binFile.Write((float)-1); // float solo_band;        // -1
+            binFile.Close();
+
+            return true;
+        }
+
+        public static double FreqConvert(double value)
+        {
+            // =LOG(A1)/LOG(2) (default = 1000 Hz)
+            return Math.Log10(value) / Math.Log10(2);
+        }
+
+        public static double QConvert(double value)
+        {
+            // =LOG(F1)*0,312098175+0,5 (default = 1)
+            return Math.Log10(value) * 0.312098175 + 0.5;
+        }
+
+    }
+
+    public enum ProQFilterType
+    {
+        Bell = 0, // (default)
+        LowShelf = 1,
+        LowCut = 2,
+        HighShelf = 3,
+        HighCut = 4,
+        Notch = 5,
+    }
+
+    public enum ProQLPHPSlope
+    {
+        Slope6dB_oct = 0,
+        Slope12dB_oct = 1,
+        Slope24dB_oct = 2, // (default)
+        Slope48dB_oct = 3,
+    }
+
+    public enum ProQStereoPlacement
+    {
+        Left = 0,
+        Right = 1,
+        Stereo = 2, // (default)
+    }
+
+    public class ProQBand
+    {
+        public ProQFilterType FilterType { get; set; }
+        public ProQLPHPSlope FilterLPHPSlope { get; set; }
+        public ProQStereoPlacement FilterStereoPlacement { get; set; }
+        public bool Enabled { get; set; }
+        public double FilterFreq { get; set; }      // value range 10.0 -> 30000.0 Hz
+        public double FilterGain { get; set; }      // + or - value in dB
+        public double FilterQ { get; set; }         // value range 0.025 -> 40.00
+
+        public override string ToString()
+        {
+            return String.Format("{0}: {1} Hz  {2} dB  Q: {3}", FilterType, FilterFreq, FilterGain, FilterQ);
+        }
+    }
+}
---
+++ REWEQ.cs
@@ -0,0 +1,207 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace PresetConverter
+{
+    /// <summary>
+    /// Read and process REW EQ filter files
+    /// </summary>
+    public static class REWEQ
+    {
+        /// <summary>
+        /// Parse a REW filters output file
+        /// The users regional locale is also taken into consideration
+        /// </summary>
+        /// <param name="filePath">path to file</param>
+        /// <returns>a rew eq filter object</returns>
+        public static REWEQFilters ReadREWEQFiltersFile(string filePath)
+        {
+
+            REWEQFilters filters = new REWEQFilters();
+
+            // Get current culture's NumberFormatInfo object.
+            NumberFormatInfo nfi = CultureInfo.CurrentCulture.NumberFormat;
+            //NumberFormatInfo nfi = CultureInfo.InvariantCulture.NumberFormat; // For debugging
+
+            // Assign needed property values to variables.
+            string decimalSeparator = nfi.NumberDecimalSeparator;
+
+            // Form regular expression pattern using the current culture's decimal seperator.
+            Regex digitsAndDecimalSeparatorOnly = new Regex(@"[^\d" + Regex.Escape(decimalSeparator) + "]");
+
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                string line;
+                int filterCount = 0;
+                string regexpPattern = @"^Filter\s+\d+";
+                bool usingBWOct = false;
+                while ((line = r.ReadLine()) != null)
+                {
+                    if (line.StartsWith("Equaliser:"))
+                    {
+                        // find out what filter parse rule to use
+                        if (line.Equals("Equaliser: FBQ2496"))
+                        {
+                            // Filter  1: ON  PEQ      Fc    64,0 Hz  Gain  -5,0 dB  BW Oct 0,167
+                            regexpPattern = @"^Filter\s+\d+:\s(\w+)\s+(\w+)\s+Fc ([\D\d" +
+                                Regex.Escape(decimalSeparator) + @"]+) Hz  Gain ([\s\d" +
+                                Regex.Escape(decimalSeparator) + @"\-]+) dB  BW Oct ([\s\d" +
+                                Regex.Escape(decimalSeparator) + @"]+)$";
+                            usingBWOct = true;
+                        }
+                        else if (line.Equals("Equaliser: Generic"))
+                        {
+                            // Filter  1: ON  PK       Fc    63,8 Hz  Gain  -5,0 dB  Q  8,06
+                            regexpPattern = @"^Filter\s+\d+:\s(\w+)\s+(\w+)\s+Fc ([\D\d" +
+                                Regex.Escape(decimalSeparator) + @"]+) Hz  Gain ([\s\d" +
+                                Regex.Escape(decimalSeparator) + @"\-]+) dB  Q ([\s\d" +
+                                Regex.Escape(decimalSeparator) + @"]+)$";
+                            usingBWOct = false;
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine("No known equaliser format!", line);
+                            return null;
+                        }
+                    }
+
+                    // skip all lines that does not start with "Filter <number>:"
+                    if (Regex.IsMatch(line, @"^Filter\s+\d+:"))
+                    {
+
+                        // remove any non breaking spaces
+                        line = Regex.Replace(line, "\xA0", String.Empty);
+
+                        // skip line if filter type is "None"
+                        if (Regex.Match(line, @"^Filter\s+\d+:\s+ON\s+None.*$").Success) continue;
+
+                        Match match = Regex.Match(line, regexpPattern);
+                        if (match.Success)
+                        {
+                            filterCount++;
+
+                            string enabled = match.Groups[1].Value.Trim();
+                            string type = match.Groups[2].Value.Trim();
+                            string freq = match.Groups[3].Value.Trim();
+                            freq = digitsAndDecimalSeparatorOnly.Replace(freq, "");
+                            string gain = match.Groups[4].Value.Trim();
+                            string q = match.Groups[5].Value.Trim();
+
+                            REWEQBand band = new REWEQBand();
+                            if (enabled.Equals("ON")) band.Enabled = true;
+                            if (type.Equals("PEQ") || type.Equals("PK")) band.FilterType = REWEQFilterType.PK;
+                            try
+                            {
+                                band.FilterFreq = Double.Parse(freq, nfi);
+                                band.FilterGain = Double.Parse(gain, nfi);
+
+                                if (usingBWOct)
+                                {
+                                    band.FilterBWOct = Double.Parse(q, nfi);
+                                    band.FilterQ = BWOct2Q(band.FilterBWOct);
+                                }
+                                else
+                                {
+                                    band.FilterQ = Double.Parse(q, nfi);
+                                    band.FilterBWOct = Q2BWOct(band.FilterQ);
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.Error.WriteLine("Parse error", e.Message);
+                                return null;
+                            }
+                            filters.EqBands.Add(band);
+                        }
+                        else
+                        {
+                            // By some reason we failed parsing the Filter line which should have worked!
+                            // probably a locale problem
+                            Console.Error.WriteLine("Could not parse line: {0}", line);
+                            return null;
+                        }
+                    }
+                }
+            }
+            return filters;
+        }
+
+
+        public static double Q2BWOct(double Qin)
+        {
+            // y =(2*E26^2+1)/(2*E26^2)+SQRT(((((2*E26^2+1)/E26^2)^2)/4)-1)
+            // =LOG10(y)/0,301
+            double Q2bw1st = ((2 * Qin * Qin) + 1) / (2 * Qin * Qin);
+            double Q2bw2nd = Math.Pow(2 * Q2bw1st, 2) / 4;
+            double Q2bw3rd = Math.Sqrt(Q2bw2nd - 1);
+            double Q2bw4th = Q2bw1st + Q2bw3rd;
+            return (Math.Log(Q2bw4th) / Math.Log(2));
+        }
+
+        public static double BWOct2Q(double q)
+        {
+            // =(SQRT(2^E18))/(2^E18-1)
+            return (Math.Sqrt(Math.Pow(2, q))) / (Math.Pow(2, q) - 1);
+        }
+    }
+
+
+    public enum REWEQFilterType
+    {
+        PK = 0, // PK for a peaking (parametric) filter
+        LP = 1, // LP for a 12dB/octave Low Pass filter (Q=0.7071)
+        HP = 2, // HP for a 12dB/octave High Pass filter (Q=0.7071)
+        LS = 3, // LS for a Low Shelf filter
+        HS = 4, // HS for a High Shelf filter
+        NO = 5, // NO for a notch filter
+        MO = 6, // Modal for a Modal filter
+
+        // The Generic and DCX2496 also have shelving filters implemented per the DCX2496
+        LS6dB = 7, // LS 6dB for a 6dB/octave Low Shelf filter
+        HS6dB = 8, // HS 6dB for a 6dB/octave High Shelf filter
+        LS12dB = 9, // LS 12dB for a 12dB/octave Low Shelf filter
+        HS12dB = 10, // HS 12dB for a 12dB/octave High Shelf filter
+
+        // The Generic equaliser setting also has
+        LPQ = 11, // LPQ, a 12dB/octave Low Pass filter with adjustable Q
+        HPQ = 12, // HPQ, a 12dB/octave High Pass filter with adjustable Q
+    }
+
+    public class REWEQBand
+    {
+        public REWEQFilterType FilterType { get; set; }
+        public bool Enabled { get; set; }
+        public double FilterFreq { get; set; }      // Hz
+        public double FilterGain { get; set; }      // dB
+        public double FilterQ { get; set; }         // Q value
+        public double FilterBWOct { get; set; }     // Bandwith per Octave
+
+        public override string ToString()
+        {
+            return String.Format("{0}: {1:0.00} Hz  {2:0.00} dB  Q: {3:0.0000}  BWOct: {4:0.0000}", FilterType, FilterFreq, FilterGain, FilterQ, FilterBWOct);
+        }
+    }
+
+    public class REWEQFilters
+    {
+
+        public List<REWEQBand> EqBands { get; set; }
+        public int Count
+        {
+            get { return EqBands.Count; }
+        }
+
+        public REWEQFilters()
+        {
+            EqBands = new List<REWEQBand>();
+        }
+
+        public IEnumerator<REWEQBand> GetEnumerator()
+        {
+            return EqBands.GetEnumerator();
+        }
+    }
+}
