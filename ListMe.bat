b8883c1f7d54f0d1fadbe4324bbf2f3431c27b24
Per Ivar Nerseth <perivar@nerseth.com>
2019-12-16 21:08:22 +0100

    Added ListMe batch scripts and swarm library

---
+++ ListMe.bat
@@ -0,0 +1,23 @@
+@ECHO off
+
+SET "CONVERTERDIR=%~dp0\PresetConverterProject\bin\Release\netcoreapp3.0\win-x64\publish"
+SET "OUTPUTDIR=%UserProfile%\My Projects\Temp"
+SET "CONVERTER=PresetConverter.exe"
+
+FOR %%i IN (%*) DO IF EXIST %%~si\NUL (CALL :ISDIR %%i) ELSE (CALL :ISFILE %%i)
+%SystemRoot%\explorer.exe "%OUTPUTDIR%"
+GOTO EXIT
+
+:ISDIR
+ECHO %1 is a directory ...
+CALL "%CONVERTERDIR%\%CONVERTER%" -i %1 -o "%OUTPUTDIR%" -l
+PAUSE
+GOTO EXIT
+
+:ISFILE
+ECHO %1 is a file ...
+CALL "%CONVERTERDIR%\%CONVERTER%" -i %1 -o "%OUTPUTDIR%" -k6 -l
+PAUSE
+GOTO EXIT
+
+:EXIT
