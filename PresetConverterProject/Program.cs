46a3846ffc51f638a9084019359380e80746eb48
Per Ivar Nerseth <perivar@nerseth.com>
2019-12-22 23:20:05 +0100

    added nicnt support

--- PresetConverterProject/Program.cs
+++ PresetConverterProject/Program.cs
@@ -22,6 +22,9 @@ namespace PresetConverter
 {
     class Program
     {
+        static readonly byte[] NKS_NICNT_MTD = new byte[] { 0x2F, 0x5C, 0x20, 0x4E, 0x49, 0x20, 0x46, 0x43, 0x20, 0x4D, 0x54, 0x44, 0x20, 0x20, 0x2F, 0x5C }; // /\ NI FC MTD  /\
+        static readonly byte[] NKS_NICNT_TOC = new byte[] { 0x2F, 0x5C, 0x20, 0x4E, 0x49, 0x20, 0x46, 0x43, 0x20, 0x54, 0x4F, 0x43, 0x20, 0x20, 0x2F, 0x5C }; // /\ NI FC TOC  /\
+
         static void Main(string[] args)
         {
             var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
@@ -67,7 +70,7 @@ namespace PresetConverter
                     logConfig.MinimumLevel.Verbose();
                     Log.Logger = logConfig.CreateLogger();
 
-                    var extensions = new List<string> { ".als", ".adv", ".vstpreset", ".xps", ".wav", ".sdir", ".cpr", ".ffp", ".nkx", ".nks", ".nkr", ".nki" }; // , ".nicnt"
+                    var extensions = new List<string> { ".als", ".adv", ".vstpreset", ".xps", ".wav", ".sdir", ".cpr", ".ffp", ".nkx", ".nks", ".nkr", ".nki", ".nicnt" };
                     var files = HandleMultipleInputPaths(optionInputDirectory, extensions);
 
                     foreach (var file in files)
@@ -1188,6 +1191,180 @@ namespace PresetConverter
                     {
                         if (!doList) NKS.ExtractEncryptedFileEntryToBf(nks, header, outBinaryFile);
                     }
+
+                    outBinaryFile.Close();
+                }
+            }
+            else if (extension == ".nicnt")
+            {
+                using (BinaryFile bf = new BinaryFile(file, BinaryFile.ByteOrder.LittleEndian, false))
+                {
+                    var header = bf.ReadBytes(16);
+                    if (header.SequenceEqual(NKS_NICNT_MTD)) // 2F 5C 20 4E 49 20 46 43 20 4D 54 44 20 20 2F 5C   /\ NI FC MTD  /\
+                    {
+                        bf.Seek(66, SeekOrigin.Begin);
+                        string version = bf.ReadString(3 * 2, Encoding.Unicode);
+                        Log.Information("Version: " + version);
+
+                        bf.Seek(132, SeekOrigin.Begin);
+                        int unknown1 = bf.ReadInt32();
+                        if (doVerbose) Log.Debug("Unknown1: " + unknown1);
+
+                        bf.Seek(144, SeekOrigin.Begin);
+
+                        int startOffset = bf.ReadInt32();
+                        Log.Information("Start Offset: " + startOffset);
+
+                        int unknown3 = bf.ReadInt32();
+                        if (doVerbose) Log.Debug("Unknown3: " + unknown3);
+
+                        bf.Seek(256, SeekOrigin.Begin);
+
+                        string productHintsXml = bf.ReadStringNull();
+                        if (doVerbose) Log.Debug("ProductHints Xml:\n" + productHintsXml);
+
+                        // the Data is an icon stored as Base64 String
+                        // https://codebeautify.org/base64-to-image-converter
+
+                        bf.Seek(startOffset + 256, SeekOrigin.Begin);
+                        var header2 = bf.ReadBytes(16);
+                        if (header2.SequenceEqual(NKS_NICNT_MTD)) // 2F 5C 20 4E 49 20 46 43 20 4D 54 44 20 20 2F 5C   /\ NI FC MTD  /\
+                        {
+                            bf.ReadBytes(116);
+
+                            long unknown4 = bf.ReadInt64();
+                            if (doVerbose) Log.Debug("Unknown4: " + unknown4);
+
+                            bf.ReadBytes(4);
+
+                            long unknown5 = bf.ReadInt64();
+                            if (doVerbose) Log.Debug("Unknown5: " + unknown5);
+
+                            bf.ReadBytes(104);
+
+                            long unknown6 = bf.ReadInt64();
+                            if (doVerbose) Log.Debug("Unknown6: " + unknown6);
+
+                            var delimiter1 = bf.ReadBytes(8);
+                            if (doVerbose) Log.Debug("Delimiter1: " + StringUtils.ByteArrayToHexString(delimiter1)); // F0 F0 F0 F0 F0 F0 F0 F0
+                            if (!delimiter1.SequenceEqual(new byte[] { 0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0 }))
+                            {
+                                Log.Error("Delimiter1 not as expected 'F0 F0 F0 F0 F0 F0 F0 F0' but got " + StringUtils.ToHexAndAsciiString(delimiter1));
+                            }
+
+                            long totalResourceCount = bf.ReadInt64();
+                            Log.Information("Total Resource Count: " + totalResourceCount);
+
+                            long totalResourceLength = bf.ReadInt64();
+                            Log.Information("Total Resource Length: " + totalResourceLength);
+
+                            var resourceList = new List<NICNTResource>();
+                            var header3 = bf.ReadBytes(16);
+                            if (header3.SequenceEqual(NKS_NICNT_TOC)) // 2F 5C 20 4E 49 20 46 43 20 54 4F 43 20 20 2F 5C  /\ NI FC TOC  /\
+                            {
+                                bf.ReadBytes(600);
+
+                                long lastIndex = 0;
+                                for (int i = 0; i < totalResourceCount; i++)
+                                {
+                                    var resource = new NICNTResource();
+
+                                    long curPos = bf.Position;
+                                    Log.Information("--------- " + curPos);
+
+                                    long resCounter = bf.ReadInt64();
+                                    Log.Information("Resource Counter: " + resCounter);
+                                    resource.Count = resCounter;
+
+                                    bf.ReadBytes(16);
+
+                                    int resUnknown = bf.ReadInt16();
+                                    if (doVerbose) Log.Debug("Resource Unknown: " + resUnknown);
+
+                                    string resName = bf.ReadString(128 * 2, Encoding.Unicode).TrimEnd('\0');
+                                    Log.Information("Resource Name: " + resName);
+                                    resource.Name = resName;
+
+                                    bf.ReadBytes(350);
+
+                                    long resIndex = bf.ReadInt64();
+                                    Log.Information("Resource Index: " + resIndex);
+                                    resource.Index = resIndex;
+
+                                    if (lastIndex > 0)
+                                    {
+                                        resource.Length = resIndex - lastIndex;
+                                    }
+                                    else
+                                    {
+                                        resource.Length = resIndex;
+                                    }
+                                    Log.Information("Resource Length: " + resource.Length);
+
+                                    lastIndex = resIndex;
+                                    resourceList.Add(resource);
+                                }
+                                Log.Information("---------");
+
+                                var delimiter2 = bf.ReadBytes(8);
+                                if (doVerbose) Log.Debug("Delimiter2: " + StringUtils.ByteArrayToHexString(delimiter2)); // F1 F1 F1 F1 F1 F1 F1 F1
+
+                                if (!delimiter2.SequenceEqual(new byte[] { 0xF1, 0xF1, 0xF1, 0xF1, 0xF1, 0xF1, 0xF1, 0xF1 }))
+                                {
+                                    Log.Error("Delimiter2 not as expected 'F1 F1 F1 F1 F1 F1 F1 F1' but got " + StringUtils.ToHexAndAsciiString(delimiter2));
+                                }
+
+                                long unknown13 = bf.ReadInt64();
+                                if (doVerbose) Log.Debug("Unknown13: " + unknown13);
+
+                                long unknown14 = bf.ReadInt64();
+                                if (doVerbose) Log.Debug("Unknown14: " + unknown14);
+
+                                var header4 = bf.ReadBytes(16);
+                                if (header4.SequenceEqual(NKS_NICNT_TOC)) // 2F 5C 20 4E 49 20 46 43 20 54 4F 43 20 20 2F 5C  /\ NI FC TOC  /\
+                                {
+                                    bf.ReadBytes(592);
+
+                                    string outputFileName = Path.GetFileNameWithoutExtension(file);
+                                    if (!doList) CreateDirectoryIfNotExist(Path.Combine(outputDirectoryPath, outputFileName));
+
+                                    foreach (var res in resourceList)
+                                    {
+                                        long curPos = bf.Position;
+                                        Log.Information(String.Format("Resource '{0}' @ position {1} [{2} bytes]", res.Name, curPos, res.Length));
+
+                                        res.Data = bf.ReadBytes((int)res.Length);
+
+                                        // if not only listing, save files
+                                        if (!doList)
+                                        {
+                                            string outputFilePath = Path.Combine(outputDirectoryPath, outputFileName, StringUtils.MakeValidFileName(res.Name));
+                                            BinaryFile outBinaryFile = new BinaryFile(outputFilePath, BinaryFile.ByteOrder.LittleEndian, true);
+
+                                            outBinaryFile.Write(res.Data);
+                                            outBinaryFile.Close();
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    Log.Error("Header4 not as expected '/\\ NI FC TOC  /\\' but got " + StringUtils.ToHexAndAsciiString(header4));
+                                }
+                            }
+                            else
+                            {
+                                Log.Error("Header3 not as expected '/\\ NI FC TOC  /\\' but got " + StringUtils.ToHexAndAsciiString(header3));
+                            }
+                        }
+                        else
+                        {
+                            Log.Error("Header2 not as expected '/\\ NI FC MTD  /\\' but got " + StringUtils.ToHexAndAsciiString(header2));
+                        }
+                    }
+                    else
+                    {
+                        Log.Error("Header not as expected '/\\ NI FC MTD  /\\' but got " + StringUtils.ToHexAndAsciiString(header));
+                    }
                 }
             }
             else
@@ -1202,7 +1379,7 @@ namespace PresetConverter
                     {
                         // var memStream = new MemoryStream();
                         // var streamWriter = new StreamWriter(memStream);
-                        
+
                         // streamWriter.WriteLine("RegistryLibraryInfo:");
                         // NKS.PrintRegistryLibraryInfo(streamWriter);
 
@@ -1210,7 +1387,7 @@ namespace PresetConverter
                         // NKS.PrintSettingsLibraryInfo(streamWriter);
                         // streamWriter.Flush();
                         // string libraryInfo = Encoding.UTF8.GetString(memStream.ToArray());
-                        
+
                         // Log.Debug(libraryInfo);
                         // memStream.Close();
 
@@ -1227,6 +1404,16 @@ namespace PresetConverter
                     Log.Error("Error processing {0} ({1})...", file, e);
                 }
             }
+        }
+
+        class NICNTResource
+        {
+            public long Count { get; set; }
+            public string Name { get; set; }
+            public long Length { get; set; }
+            public byte[] Data { get; set; }
+            public long Index { get; set; }
+            public long RealIndex { get; set; }
         }
 
         private static void CreateDirectoryIfNotExist(string filePath)
